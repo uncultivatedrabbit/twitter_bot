296cbec187b735906629dd6a165b6b18fa404762
uncultivatedrabbit <45187253+uncultivatedrabbit@users.noreply.github.com>
2019-05-02 16:16:16 -0400

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:         'mcJPXpy7c4BmpfM89p0KfMyuN',
-  consumer_secret:      'Mvm3c2Yuk1qsnWShAldE3NuMu0wbdeV15cR8Q8tL9XNUzV3p9m',
-  access_token:         '1112097593436311552-G0LYiBnFPc99iFaOv5C1mTH4vB3h8Y',
-  access_token_secret:  'fobYy5QRviBI8i3wS0Og4pe1kJ6dwDjPsv3in4agKK1dS'
-};
