3545b4b7d285d484b61626ab5a183b9af6ce01ad
uncultivatedrabbit <uncultivatedrabbit@gmail.com>
2019-05-23 14:07:08 -0400

    Make the bot more secure and remove interval

--- bot.js
+++ bot.js
@@ -3,27 +3,29 @@ console.log("The bot is starting...");
 const Twit = require ("twit");
 //requires twitter config information, eg consumer key, access tokens from config.js
 const config = require("./config");
-const tweetIt = require("./tweetStack")
+const tweetIt = require("./tweetStack");
 
 const T = new Twit(config);
-const day = 86400000;
+// const day = 86400000;
 
 launchTweets();
 //sets tweet interval to once a day
-let interval = setInterval(launchTweets, day)
+// let interval = setInterval(launchTweets, day)
 
 
 function launchTweets(){
 
   // checks if there are any tweets left
   if (tweetIt.tweetStack.length === 0){
-    clearInterval(interval);
+    // clearInterval(interval);
     console.log("There's nothing left to tweet");
     } else {
       // randomizes the tweets
     let randomize = Math.floor(Math.random()* tweetIt.tweetStack.length);
     // tests that the tweets on interval are working
       console.log(tweetIt.tweetStack[randomize]);
+
+
 
     let tweet = {
       status: tweetIt.tweetStack[randomize]
